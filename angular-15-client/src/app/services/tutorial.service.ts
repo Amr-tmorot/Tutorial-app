eb018a32b1c1c615d23fc53c7f2ad29b8ebaf4de
gregcabrera422 <gregcabrera422@gmail.com>
2023-09-08 17:35:04 +0800

    Update tutorial.service.ts
    update http to https

--- angular-15-client/src/app/services/tutorial.service.ts
+++ angular-15-client/src/app/services/tutorial.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 
-const baseUrl = 'http://<host-name>/api/tutorials';
+const baseUrl = 'https://<host-name>/api/tutorials';
 
 @Injectable({
   providedIn: 'root'
