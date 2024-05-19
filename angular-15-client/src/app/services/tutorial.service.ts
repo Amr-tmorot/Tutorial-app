121c04dc1c8927dba4d6a1030519dc9bc741efb2
amr-tmorot <amroessameldin@gmail.com>
2024-05-19 15:57:35 +0000

    adjust the host in the angular baseURL to connect to the backend server

--- angular-15-client/src/app/services/tutorial.service.ts
+++ angular-15-client/src/app/services/tutorial.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 
-const baseUrl = 'https://<host-name>/api/tutorials';
+const baseUrl = 'https://backend-svc:8080/api/tutorials';
 
 @Injectable({
   providedIn: 'root'
